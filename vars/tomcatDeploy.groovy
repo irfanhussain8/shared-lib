72d6edd809e2907973d5f960231ea65a3c67f8ec
irfanhussain8 <95165409+irfanhussain8@users.noreply.github.com>
2022-06-08 19:51:22 +0530

    Update and rename tomcat-deploy.groovy to tomcatDeploy.groovy

---
+++ vars/tomcatDeploy.groovy
@@ -0,0 +1,10 @@
+def call(credId,tomcatIp,tomcatuserId){
+sshagent(['credId']){
+    // copy war file to tomcat server
+    sh """
+    scp -o StrictHostKeyChecking=no target/myweb*.war $(tomcatuserId)@$(tomcatIp):/opt/tomcat8/webapps/myweb.war
+    ssh $(tomcatuserId)@$(tomcatIp) /opt/tomcat8/bin/shutdown.sh
+    ssh $(tomcatuserId)@$(tomcatIp) /opt/tomcat8/bin/startup.sh
+       """
+  }
+}
