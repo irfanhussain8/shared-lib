1fbc039404a96b2134b184c7fda1dc9ac0b8f579
irfanhussain8 <95165409+irfanhussain8@users.noreply.github.com>
2022-06-09 12:24:22 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,10 +1,10 @@
-def call(credId,tomcatIp,tomcatuserId){
-sshagent([credId]){
+def call(credId,tomcatIp,tomcatUser){
+  sshagent([credId]) {
     // copy war file to tomcat server
     sh """
-    scp -o StrictHostKeyChecking=no target/myweb*.war $(tomcatuserId)@$(tomcatIp):/opt/tomcat8/webapps/myweb.war
-    ssh $(tomcatuserId)@$(tomcatIp) /opt/tomcat8/bin/shutdown.sh
-    ssh $(tomcatuserId)@$(tomcatIp) /opt/tomcat8/bin/startup.sh
-       """
+        scp -o StrictHostKeyChecking=no target/myweb*.war ${tomcatUser}@${tomcatIp}:/opt/tomcat8/webapps/myweb.war
+        ssh ${tomcatUser}@${tomcatIp} /opt/tomcat8/bin/shutdown.sh
+        ssh ${tomcatUser}@${tomcatIp} /opt/tomcat8/bin/startup.sh
+    """
   }
 }
